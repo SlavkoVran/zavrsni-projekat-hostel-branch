7897ee7aed050128f3e3df7d1d6cecce5374dfc7
Slavko Vranešević <44097277+SlavkoVran@users.noreply.github.com>
2019-04-28 01:50:12 +0200

    Add files via upload

--- js/rezervacija-modal.js
+++ js/rezervacija-modal.js
@@ -11,24 +11,20 @@ $('#btnNazadDatum').on('click', function () {
 });
 
 $('#btnNazadRezervacija').on('click', function () {
-    $('.modal-soba').hide();
-    $('.modal-datum').show();
+    $('.modal-soba').css('display', 'none');
+    $('.modal-datum').css('display', 'flex');
 });
 
 $('#btnPrikaziRezervaciju').on('click', function () {
-    $('.modal-soba').hide();
-    $('.ispis-rezervacije').show();
+    $('.modal-soba').css('display', 'none');
+    $('.ispis-rezervacije').css('display', 'flex');
 });
 
-$('#btnNazadPrikaz').on('click', function () {
-    $('.ispis-rezervacije').hide();
-    $('.modal-soba').show();
-    console.log("radi");
 
-});
 
-//Fracanje na pocetnu stranu
 
+
+//Vracanje na pocetnu stranu
 $('#btnDaljeDatum').on('click', function () {
 
     var datumPrijave = $('#datumPrijave').val();
@@ -36,7 +32,7 @@ $('#btnDaljeDatum').on('click', function () {
     if (datumPrijave != 0) {
         $('.poruka-datum').hide();
         $('.modal-datum').css('display', 'none');
-        $('.modal-soba').show();
+        $('.modal-soba').css('display', 'flex');
     }
     else {
         $('.poruka-datum').text('Niste uneli validan datum').show();
@@ -47,61 +43,52 @@ $('#btnDaljeDatum').on('click', function () {
 
 // Rezervacija klasa 
 class Rezervacija {
-    constructor(ime, telefon, email, datumPrijave, datumOdjave) {
+    constructor(ime, telefon, email, datumPrijave, datumOdjave, soba, brKreveta) {
         this.ime = ime;
         this.telefon = telefon;
         this.email = email;
         this.datumPrijave = datumPrijave;
         this.datumOdjave = datumOdjave;
-        // this.brKreveta = brKreveta;
+        this.soba = soba;
+        this.brKreveta = brKreveta;
     }
 }
 
 // KI klas, korisnicki interfejs
 
 class KI {
-    // static prikaziRezervaciju() {
-
-    //     const SnimljeneRezervacije = [
-    //         {
-    //             ime: 'Slavko Vranesevic',
-    //             telefon: '066000699',
-    //             email: 'primer@primer.com',
-    //             datumPrijave: '07.05.2019',
-    //             datumOdjave: '15.05.2019',
-    //             brKreveta: '2'
-    //         }, 
-
-    //         {
-    //             ime: 'Pera Peric',
-    //             telefon: '0000000',
-    //             email: 'primer@primer.com',
-    //             datumPrijave: '07.05.2019',
-    //             datumOdjave: '15.05.2019',
-    //             brKreveta: '4'
-    //         }
-    //     ];
-
-
-    //     const rezervacije = SnimljeneRezervacije;
-
-    //     rezervacije.forEach((rez) => KI.dodajRezervacijuUlistu(rez));
-    // }
+    
 
     static dodajRezervacijuUlistu(rez) {
-        const lista = document.querySelector('#rezervacija-lista');
+        const listaKorisnik = document.querySelector('#lista-korisnik');
+        const listaRezervacija = document.querySelector('#lista-rezervacija');
+        const ispisNaslov = document.querySelector('#ispis-naslov');
+        const naslov = document.createElement('h3');
+        const redKorisnik = document.createElement('tr');
+        const redRezervacija = document.createElement('tr');
+        
 
-        const red = document.createElement('tr');
-
-        red.innerHTML = `
+        redKorisnik.innerHTML = `
         <td>${rez.ime}</td>
         <td>${rez.telefon}</td>
         <td>${rez.email}</td>
-        <td>${rez.datumPrijave}</td>
-        <td>${rez.datumOdjave}</td>
+
         `;
 
-        lista.appendChild(red);
+        redRezervacija.innerHTML = `
+        <td>${rez.datumPrijave}</td>
+        <td>${rez.datumOdjave}</td>
+        <td>${rez.soba}</td>
+        <td>${rez.brKreveta}</td>
+        `;
+
+        naslov.innerHTML = `
+        Uneli ste sledece podatke za rezervaciju
+        `;
+
+        listaKorisnik.appendChild(redKorisnik);
+        listaRezervacija.appendChild(redRezervacija);
+        ispisNaslov.appendChild(naslov);
     };
 
 
@@ -128,11 +115,12 @@ document.getElementById("btnPrikaziRezervaciju").addEventListener('click', (e) =
     const email = document.getElementById('email').value;
     const datumPrijave = document.getElementById('datumPrijave').value;
     const datumOdjave = document.getElementById('datumOdjave').value;
-    // const brKreveta = document.getElementById('brKreveta').value;
+    const soba = document.getElementById('izborSoba').value;
+    const brKreveta = document.getElementById('brKreveta').value;
     
 
    // kreiranjene rezervacije 
-   const rezervacija = new Rezervacija (ime, telefon, email, datumPrijave, datumOdjave);
+   const rezervacija = new Rezervacija (ime, telefon, email, datumPrijave, datumOdjave, soba, brKreveta);
    
 
    //prikaz korisniku
@@ -140,74 +128,37 @@ document.getElementById("btnPrikaziRezervaciju").addEventListener('click', (e) =
 
 }); 
 
+//Slanje forme namail
+document.getElementById("btnPosalji").addEventListener('click', (e) =>{
+
+    //Prevent actual submit
+    e.preventDefault();
+
+
+    // hvatanje unetih vrednosti
+    const ime = document.getElementById('ime').value;
+    const telefon = document.getElementById('telefon').value;
+    const email = document.getElementById('email').value;
+    const datumPrijave = document.getElementById('datumPrijave').value;
+    const datumOdjave = document.getElementById('datumOdjave').value;
+    const soba = document.getElementById('izborSoba').value;
+    const brKreveta = document.getElementById('brKreveta').value;
+
+    let slanjeIme = document.getElementById('slanjeIme');
+    slanjeIme = ime;
+    console.log(slanjeIme);
+
+   
+   
+
+}); 
+
+
+
 //Event:  otkazi rezervaciju
 document.getElementById('btnOtkazi').addEventListener('click',(e) =>{
 window.location = "./index.html";
 });
 
-// document.getElementById('btnOtkazi').addEventListener('click', location.reload());
 
-
-
-
-
-
-// class Soba{
-//     constructor(nazivSoba, brKreveta){ 
-//     this.nazivSoba = nazivSoba;
-//     this.brKreveta = brKreveta;
-//     }
-
-//     izborSoba (){
-
-//         let nizBrojKreveta = new Array();
-//         // let nizSoba = new Array();;
-//         var rezBrojKrevetaA = document.getElementById('brojKrevetaA').value;
-//         var rezBrojKrevetaB = document.getElementById('brojKrevetaB').value;
-//         var rezBrojKrevetaC = document.getElementById('brojKrevetaC').value;
-//         var rezBrojKrevetaD = document.getElementById('brojKrevetaD').value;
-
-
-
-//         this.brKreveta = nizBrojKreveta.push(rezBrojKrevetaA, rezBrojKrevetaB, rezBrojKrevetaC, rezBrojKrevetaD);
-
-//         }
-
-// }
-
-
-// class Rezervacija extends Soba{
-//     constructor (nazivSoba, brKreveta, datumPrijave, datumOdjave){
-//         super(nazivSoba, brKreveta)
-//     this.datumPrijave = datumPrijave;
-//     this.datumOdjave = datumOdjave;
-//     }
-
-
-// }
-
-// class Kupac {
-//     constructor(ime, telefon, email){
-//     this.ime = ime;
-//     this.telefon = telefon;
-//     this.email = email;
-//     }
-
-
-
-// }
-
-// /
-// let sobaA = ' Soba "A" ';
-// let sobaB = ' Soba "B" ';
-// let sobaC = ' Soba "C" ';
-// let sobaD = ' Soba "D" ';
-
-// soba1 = new Soba ("SobaA", 2);
-// console.log(soba1);
-
-// kupac1 = new Kupac ("Pera Peric", 064/3682548, "slavko@gmail.com");
-
-// rezervacija1 = new Rezervacija ("SobaA", 2, 12.03, 13.03, kupac1);
-// console.log(rezervacija1);
 
