0153fb1bafb7b007741147c0103d9d816ddb25cb
Slavko Vranešević <44097277+SlavkoVran@users.noreply.github.com>
2019-05-03 23:19:37 +0200

    Add files via upload

--- js/rezervacija.js
+++ js/rezervacija.js
@@ -1,147 +1,106 @@
-
-var formValidacija = {
-    ime: false,
-    email: false,
-    telefon: false,
-};
-
-
-// funkcija koja proverava da li su svi uslovi validirani odnosno
-//  da li je forma ispravno popunjena i prikaz poruke 
-//o gresci ukoliko nije dobro popunjeno 
-function proveriValidaciju(){
-if (formValidacija.ime && formValidacija.email  && formValidacija.telefon){
-    $('#btnDaljeKorisnik').removeAttr('disabled');
-}
-else {
-    $('#btnDaljeKorisnik').attr('disabled', true);
-}
-};
-
-//funkcija za prikaz poruke ukoliko nisu uneti validni podaci
-function prikaziPoruku(sadrzaj){ 
-    $('.poruka-ime').text(sadrzaj).show();
-};
-
-//funkcija koja ukoliko su uneti validni podaci ne ispisuje poruku
-function sakrijPoruku(){
-    $('.poruka-ime').hide();
-}
-
-// validacija unosa imena
-$('#ime').on('input', function(){
-var ime = $(this).val();
-
-if (ime.length < 1){
-    prikaziPoruku('Polje je prazno! Unesite ime');
-    formValidacija.ime = false;
-    proveriValidaciju();
-}
-else{
-    sakrijPoruku();
-    formValidacija.ime = true;
-    proveriValidaciju();
-    var testValidacijaIme = new RegExp(/^[A-ZŽČĆŠĐ][a-zđšžćč]{1,11}(\s[A-ZŽČĆŠĐ][a-zđšžćč]{1,11})+$/);
-    if(!testValidacijaIme.test(ime)){
-        prikaziPoruku('Mozete koristiti samo mala i velika slova');
-        formValidacija.ime = false;
-        proveriValidaciju();
-    }
-    else{
-sakrijPoruku();
-formValidacija.ime = true;
-proveriValidaciju();
-
+// Rezervacija klasa 
+class Rezervacija {
+    constructor(ime, telefon, email, datumPrijave, datumOdjave, soba, brKreveta) {
+        this.ime = ime;
+        this.telefon = telefon;
+        this.email = email;
+        this.datumPrijave = datumPrijave;
+        this.datumOdjave = datumOdjave;
+        this.soba = soba;
+        this.brKreveta = brKreveta;
     }
 }
-});
 
-//validacija unosa datuma
-$('#btnDaljeDatum').on('click', function () {
-    var datumPrijave = $('#datumPrijave').val();
-    if (datumPrijave != 0) {
-        $('.poruka-datum').hide();
-        $('.modal-datum').css('display', 'none');
-        $('.modal-soba').css('display', 'flex');
-    }
-    else {
-        $('.poruka-datum').text('Niste uneli validan datum').show();
-    }
-});
+// KI klas, korisnicki interfejs
+class KI {
+    static dodajRezervacijuUlistu(rez) {
+        const listaKorisnik = document.querySelector('#lista-korisnik');
+        const ispisNaslov = document.querySelector('#ispis-naslov');
+        const naslov = document.createElement('h3');
+        const redKorisnik = document.createElement("div");
+        
+        redKorisnik.innerHTML = `
+        <span> ${rez.ime} </span><br>
+        <span> ${rez.telefon} </span><br>
+        <span> ${rez.email} </span><br>
+        <span> ${rez.datumPrijave} </span><br>
+        <span> ${rez.datumOdjave} </span><br>
+        <span> ${rez.soba} </span><br>
+        <span> ${rez.brKreveta} </span><br>
+        </div>`;
+        naslov.innerHTML = `Uneli ste sledece podatke za rezervaciju`;
 
-
-
-// provera validnosti input polja #email
-$('#email').on('input', function(){
-    var email = $(this).val();
-    
-    function prikaziPoruku(sadrzaj){ 
-        $('.poruka-email').text(sadrzaj).show();
+        listaKorisnik.appendChild(redKorisnik);
+        ispisNaslov.appendChild(naslov);
     };
-    
-    function sakrijPoruku(){
-        $('.poruka-email').hide();
+};
+
+// Dodavanej nove rezervacije
+document.getElementById("btnPrikaziRezervaciju").addEventListener('click', (e) => {
+
+    //Prevent actual submit
+    e.preventDefault();
+
+    // hvatanje unetih vrednosti
+    const ime = document.getElementById('ime').value;
+    const telefon = document.getElementById('telefon').value;
+    const email = document.getElementById('email').value;
+    const datumPrijave = document.getElementById('datumPrijave').value;
+    const datumOdjave = document.getElementById('datumOdjave').value;
+    const brKreveta = Number(document.getElementById('brKreveta').value);
+    const soba = document.getElementById('izborSoba').value;
+
+    function izabranTekst() {
+        var elt = document.getElementById('izborSoba');
+
+        if (elt.selectedIndex == -1)
+            return null;
+
+        return elt.options[elt.selectedIndex].text;
     }
-    
-    if (email.length < 1){
-        prikaziPoruku('Polje je prazno! Unesite email');
-        formValidacija.email = false;
-        proveriValidaciju();
+
+    var izabranaSoba = izabranTekst('izborSoba');
+    // validacija izbora sobe i broja kreveta
+    if (soba == '' || brKreveta == ''){
+        prikaziPoruku('Izaberite sobu i broj kreveta');
     }
-    else{
+    else if (brKreveta > soba) {
         sakrijPoruku();
-        formValidacija.email = true;
-        proveriValidaciju();
-        var testValidacijaEmail = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$/);
-        if(!testValidacijaEmail.test(email)){
-            prikaziPoruku('Unesite email u formi test@test.com');
-            formValidacija.email = false;
-            proveriValidaciju();
-        }
-        else{
-    sakrijPoruku();
-    formValidacija.email = true;
-    proveriValidaciju();
-
-        }
+        prikaziPoruku('Broj kreveta ne može biti veći od kapaciteta sobe.')
     }
-    });
-
-//provera validnosti input polja #telefon
-$('#telefon').on('input', function(){
-    var telefon = $(this).val();
-
-    
-    
-    if (telefon.length < 1){
-        prikaziPoruku('Polje je prazno! Unesite broj telefona');
-        formValidacija.telefon = false;
-        proveriValidaciju();
-    
-    }
-    else{
+    else 
+    {
         sakrijPoruku();
-        formValidacija.telefon = true;
-        var testValidacijaTelefon = new RegExp(/\d{3}\/(\d){3}[\-]?(\d){3}/);
-        if(!testValidacijaTelefon.test(telefon)){
-            prikaziPoruku('Unesite telefon u formatu XXX/XXX-XXX-XX');
-            formValidacija.telefon = false;
-        }
-        else{
-   sakrijPoruku();
-    if(telefon.length > 11 ){
-        prikaziPoruku('Uneli ste previse cifara.');
-        formValidacija.telefon =false;
-        proveriValidaciju();
+        // kreiranjene rezervacije 
+        const rezervacija = new Rezervacija(ime, telefon, email, datumPrijave, datumOdjave, izabranaSoba, brKreveta);
+
+        //prikaz korisniku
+        KI.dodajRezervacijuUlistu(rezervacija);
+
+        //prikaz unetih podatak
+        document.querySelector('.modal-soba').style.display = 'none';
+        document.querySelector('.ispis-rezervacije').style.display = 'flex';
+        // $('.modal-soba').css('display', 'none');
+        // $('.ispis-rezervacije').css('display', 'flex');
     }
-    else{
-        sakrijPoruku();
-        formValidacija.telefon = true;
-        proveriValidaciju();
-        }
-        }
-    }
-    });
+});
+
+// pozdravna poruka
+document.getElementById('btnPotvrdi').addEventListener('click', function () {
+    document.querySelector('.porukaHvala').style.display = 'flex';
+
+    document.querySelector('#ispis-naslov').style.display = 'none';
+    document.querySelector('#ispis').style.display = 'none';
+  
+    setTimeout(function () {
+        window.location = "./index.html";
+    }, 1000);
+});
+
+//Event:  otkazi rezervaciju, vracanje na pocetnu stranu 
+document.getElementById('btnOtkazi').addEventListener('click', (e) => {
+    window.location = "./index.html";
+});
 
 
-   
+
